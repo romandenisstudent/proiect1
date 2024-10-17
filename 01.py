cb6b1e34fd7bfb34d85aac9a299865f0b01ca9e8
romandenisstudent <Roman.Io.Denis@student.utcluj.ro>
2024-10-17 11:22:13 +0300

    primul program

--- 01.py
+++ 01.py
@@ -1,1 +1,8 @@
-print("Hello world")
+print("Welcome to my project")
+varsta_mea=19
+data_nasterii=2005
+print(varsta_mea+data_nasterii)
+ceva=1024
+impartitorul=4
+print(ceva/impartitorul)
+print(data_nasterii-varsta_mea+ceva/impartitorul)
